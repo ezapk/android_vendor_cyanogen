349043438e20b64dab7a5e6925cb3be44eab6652
Dewayne <playfulgod@gmail.com>
2011-09-30 01:46:58 -0300

    updated

--- products/cyanogen_ascend2.mk
+++ products/cyanogen_ascend2.mk
@@ -1,4 +1,5 @@
-# Inherit AOSP device configuration for Ascend.
+ 
+# Inherit AOSP device configuration for m865.
 $(call inherit-product, device/huawei/ascend2/ascend2.mk)
 
 # Inherit some common cyanogenmod stuff.
@@ -7,18 +8,22 @@ $(call inherit-product, vendor/cyanogen/products/common_full.mk)
 # Include GSM stuff
 $(call inherit-product, vendor/cyanogen/products/gsm.mk)
 
+#
+# Setup device specific product configuration.
+#
 PRODUCT_NAME := cyanogen_ascend2
 PRODUCT_BRAND := Huawei
 PRODUCT_DEVICE := ascend2
 PRODUCT_MODEL := M865
 PRODUCT_MANUFACTURER := Huawei
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE=hwm865 PRODUCT_NAME=M865 BUILD_ID=GINGERBREAD BUILD_DISPLAY_ID=$(shell date +%m%d%Y) BUILD_FINGERPRINT=Huawei/M865/hwm865:2.3.5/HuaweiM865/C153B826:user/ota-rel-keys,release-keys
+PRODUCT_BOARD := M865
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=M865 BUILD_ID=GINGERBREAD BUILD_DISPLAY_ID=CM7A2-$(shell date +%m%d%Y) BUILD_FINGERPRINT=Huawei/M865/hwm865:2.3.5/HuaweiM865/PlayfulGod:user/ota-rel-keys,release-keys PRODUCT_DEVICE=hwm865
 
-#PRODUCT_SPECIFIC_DEFINES += TARGET_PRELINKER_MAP=$(TOP)/vendor/cyanogen/prelink-linux-arm-ascend2.map
 
 #
 # Set ro.modversion
 #
+
 ifdef CYANOGEN_NIGHTLY
     PRODUCT_PROPERTY_OVERRIDES += \
         ro.modversion=CM7.2-M865-Nightly$(shell date +%m%d%Y)
@@ -35,16 +40,6 @@ endif
 #
 # Copy legend specific prebuilt files
 #
-# Lights and graphics
+# Bootanimation
 PRODUCT_COPY_FILES += \
-   vendor/huawei/ascend2/proprietary/lib/hw/gralloc.msm7k.so:system/lib/hw/gralloc.msm7k.so
-
-PRODUCT_COPY_FILES += \
-    vendor/huawei/ascend2/proprietary/lib/hw/lights.msm7k.so:system/lib/hw/lights.msm7k.so
-
-# Proprietary RIL related
-PRODUCT_COPY_FILES += \
-    vendor/huawei/ascend2/proprietary/lib/libril.so:system/lib/libril.so
-PRODUCT_COPY_FILES += \
-    vendor/huawei/ascend2/proprietary/lib/libwpa_client.so:system/lib/libwpa_client.so
-
+    device/huawei/ascend2/include/media/bootanimation.zip:system/media/bootanimation.zip
