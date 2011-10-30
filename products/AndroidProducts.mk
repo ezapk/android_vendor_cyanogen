0bb88fba77453cc7fcf748aca900833c6daaaca9
shunyea <shunyea@msn.com>
2011-10-30 17:58:36 +0800

    Update products/AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,15 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_ascend2.mk \
     $(LOCAL_DIR)/cyanogen_ascend.mk \
-    $(LOCAL_DIR)/cyanogen_a70.mk \
     $(LOCAL_DIR)/cyanogen_m835.mk \
-    $(LOCAL_DIR)/cyanogen_thunderc.mk \
-    $(LOCAL_DIR)/cyanogen_thunderc_cricket.mk \
-    $(LOCAL_DIR)/cyanogen_thunderc_metropcs.mk \
-    $(LOCAL_DIR)/cyanogen_thunderc_sprint.mk \
-    $(LOCAL_DIR)/cyanogen_thunderc_usc.mk \
-    $(LOCAL_DIR)/cyanogen_thunderc_verizon.mk \
-    $(LOCAL_DIR)/cyanogen_thunderc_virgin.mk \
 
 
 
